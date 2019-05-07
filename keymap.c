88cc019532206771e24565192b76099b8709dae9
shin <shin@dova.co.jp>
2019-05-07 16:38:36 +0900

    change keymap.

--- keymap.c
+++ keymap.c
@@ -35,29 +35,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,   KC_1,    KC_2,    KC_3,   KC_4,    KC_5,  KC_MINS,          KC_EQL,   KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR, \
     KC_ESC,   KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,  KC_LBRC,          KC_RBRC,  KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_TAB,   KC_A,    KC_S,    KC_D,   KC_F,    KC_G,  KC_LEFT,          KC_RIGHT, KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LANG5, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,  KC_UP,            KC_DOWN,  KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_PGUP, \
-    KC_LANG1, KC_LGUI, KC_LALT, KC_DEL, KC_LSFT, RAISE, KC_SPC,           KC_ENT,   LOWER, KC_RCTL, KC_BSPC, KC_HOME, KC_END,  KC_PGDN
+    KC_HENK,  KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,  KC_UP,            KC_DOWN,  KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_PGUP, \
+    KC_MHEN,  KC_LGUI, KC_LALT, KC_DEL, KC_LSFT, RAISE, KC_SPC,           KC_ENT,   LOWER, KC_RCTL, KC_BSPC, KC_HOME, KC_END,  KC_PGDN
   ),
 
   /* Lower
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |   {  |                    |   }  |  F6  |  F7  |  F8  |  F9  |  F10 |  F12 |
+   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |  ~   |   !  |   @  |   #  |   $  |   %  |   _  |                    |   +  |   ^  |   &  |   *  |   (  |   )  |  |   |
+   * |      |      |      |      |      |      |      |                    |      | NumLk|   7  |   8  |   9  |   -  |      |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   1  |   2  |   3  |   4  |   5  |  Del |                    | Bksp |   H  |   J  |   K  |   L  |   :  |  "   |
+   * |      |      |      |      |      |      |      |                    |      |   *  |   4  |   5  |   6  |   +  |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   6  |   7  |   8  |   9  |   0  | Space|                    | Enter|   N  |   M  |   <  |   >  |   ?  | Shift|
+   * |      |      |      |      |      |      |      |                    |      |   /  |   1  |   2  |   3  |  Ent |      |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | EISU |||||||| Lower| Space|      ||||||||      | Enter| Raise|||||||| Home |PageDn|PageUp|  End |
+   * |      |      |      |      |      | Raise|      |      ||||||||      |      | Lower|      |      |      |      |      |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT(
-    KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
-    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL ,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQT , \
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC ,KC_SPC,          KC_SPC,KC_ENT , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_NO,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,            KC_F7,  KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12,  KC_NO, \
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,            KC_NO,  KC_NLCK, KC_7,   KC_8,   KC_9,   KC_PMNS, KC_NO, \
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,            KC_NO,  KC_PAST, KC_4,   KC_5,   KC_6,   KC_PPLS, KC_NO, \
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,            KC_NO,  KC_PSLS, KC_1,   KC_2,   KC_3,   KC_PENT, KC_NO, \
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  RAISE,  KC_NO,            KC_NO,  LOWER,   KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO
   ),
 
   /* Raise
